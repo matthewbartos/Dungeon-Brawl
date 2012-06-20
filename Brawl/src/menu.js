63d6fceaa86049a4c24f628a74828fb01687d23b
SuperJasz <SuperJasz@Killer-Machine.home>
2012-06-20 20:34:06 +0200

    Correcting button.

--- Brawl/src/menu.js
+++ Brawl/src/menu.js
@@ -3,6 +3,15 @@
 window.onresize = function(){
     
 }
-
+//bootstrap
 window.onload = function(){
+    //declaring public HTML elements
+    //@type HTMLDocument
+    dMenu = document.getElementById("dMenu");
+    dGame = document.getElementById("dGame");
+    //setting up listeners
+    document.getElementById("bStart").onclick = function(){
+        dGame.hidden = false;
+        dMenu.hidden = true;
+    }
 }
