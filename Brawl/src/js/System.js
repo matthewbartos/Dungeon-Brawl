ee1857c4bb11842ac63cfa19b5124b9930a7c260
SuperJasz <SuperJasz@Killer-Machine.home>
2012-06-23 22:40:02 +0200

    Basic image cutting.

--- Brawl/src/js/System.js
+++ Brawl/src/js/System.js
@@ -7,6 +7,28 @@ Map = {
      * @param {Object} map Map to parse
      */
     parseMap: function(map){
-        console.log(typeof map);
+        map.tilesets.forEach(function(i){
+            //checking if it's not the object layer
+            if(i.name != "Objects"){
+                var img = new Image();
+                //needs change
+                img.src = "graphics" + i.image.substring(11,i.image.length);
+                //cutting the image
+                img.onload = function(){
+                    imagePieces = [];
+                    var canvas = document.createElement('canvas');
+                    var context = canvas.getContext('2d');
+                    canvas.width = canvas.height = 32;
+                    for(var y = 0; y < i.imageheight/32; ++y) {
+                        for(var x = 0; x < i.imagewidth/32; ++x) {
+                            canvas.width = canvas.height = 32;
+                            context.drawImage(img, x * 32, y * 32, 32, 32, 0, 0, 
+                            canvas.width, canvas.height);
+                            imagePieces.push(canvas.toDataURL());
+                        }
+                    }
+                }
+            }
+        });
     }
 }
