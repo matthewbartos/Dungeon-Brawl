8252077f80f6c8be5afbb28a814b21f219b1b721
Jasz <>
2012-06-24 10:51:57 +0200

    Saving Bitmaps instead of URL:data and saving only those Bitmaps that are actually used.

--- Brawl/src/js/System.js
+++ Brawl/src/js/System.js
@@ -7,6 +7,14 @@ Map = {
      * @param {Object} map Map to parse
      */
     parseMap: function(map){
+        //checking which tiles will be used
+        var used = [];
+        map.layers.forEach(function(i){
+            if(i.name.indexOf("obj") != -1) return;
+            for(var j in i.data){
+                used[parseInt(i.data[j])] = true;
+            }
+        });
         map.tilesets.forEach(function(i){
             //checking if it's not the object layer
             if(i.name != "Objects"){
@@ -15,20 +23,31 @@ Map = {
                 img.src = "graphics" + i.image.substring(11,i.image.length);
                 //cutting the image
                 img.onload = function(){
-                    imagePieces = [];
+                    bitmapArray = [];
+                    var index = parseInt(i.firstgid); //(SIC)
                     var canvas = document.createElement('canvas');
                     var context = canvas.getContext('2d');
                     canvas.width = canvas.height = 32;
-                    for(var y = 0; y < i.imageheight/32; ++y) {
-                        for(var x = 0; x < i.imagewidth/32; ++x) {
+                    for(var y = 0; y < i.imageheight/32; ++y){
+                        w:
+                        for(var x = 0; x < i.imagewidth/32; ++x){
+                            if(!used[index]){
+                                ++index;
+                                continue w;
+                            }
                             canvas.width = canvas.height = 32;
                             context.drawImage(img, x * 32, y * 32, 32, 32, 0, 0, 
-                            canvas.width, canvas.height);
-                            imagePieces.push(canvas.toDataURL());
+                                canvas.width, canvas.height);
+                            var imgTemp = new Image();
+                            imgTemp.src = canvas.toDataURL();
+                            bitmapArray[index] = new Bitmap(imgTemp);
+                            ++index;
                         }
                     }
                 }
             }
+            
         });
+        
     }
 }
